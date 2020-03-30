816e8fdbd6989c1986499b19d745339d84ab2a15
bats6789 <bats23456789@gmail.com>
2020-03-29 19:33:45 -0500

    Added PDFSplitter and PDFMerger

---
+++ src/main/java/edu/uab/cwingard/pdfeditor/PDFMerger.java
@@ -0,0 +1,77 @@
+/*
+ * File: PDFSplitter.java
+ * Author: Blake Wingard, cwingard@uab.edu
+ * Assignment: group project - EE333 Spring 2020
+ * Vers: 1.0.0 03/29/2020 cbw - Initial coding
+ */
+package edu.uab.cwingard.pdfeditor;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.apache.pdfbox.multipdf.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+/**
+ * Merges multiple pdf's into a single pdf.
+ * 
+ * @author Blake Wingard - cwingard@uab.edu
+ */
+public class PDFMerger {
+    public static void main( String[] args ) throws IOException{
+        String outputFilename = null;
+        ArrayList<String> inputFilenames = new ArrayList<>();
+        File outputFile = null;
+        ArrayList<File> inputFiles = new ArrayList<>();
+        PDDocument outputPDF = null;
+        ArrayList <PDDocument> inputPDFs = new ArrayList<>();
+        PDFMergerUtility pdfMerger = new PDFMergerUtility();
+        int fileCount = 0;
+        int outputFlag = 0;
+        
+        if( args.length < 4 ){
+            System.err.println( "ERROR: Not enough inputs" );
+        } else {
+            for( String arg : args ){
+                if( arg.equals( "-o" ) ){
+                    ++outputFlag;
+                } else if( outputFlag == 1 ){
+                    outputFilename = arg;
+                    ++outputFlag;
+                } else if( outputFlag > 2 ){
+                    System.err.println( "ERROR: To many output flags." );
+                    System.exit( -1 );
+                } else {
+                    inputFilenames.add( arg );
+                }
+            }
+            try{
+                outputPDF = new PDDocument();
+                for( String inputFilename : inputFilenames ){
+                    inputFiles.add( new File( inputFilename ));
+                    inputPDFs.add( PDDocument.load( inputFiles.get( fileCount )));
+                    fileCount++;
+                }
+            } catch( FileNotFoundException ex ){
+                System.err.println( "ERROR: Failed to open: " + ex.toString());
+            } catch( IOException ex ) {
+                Logger.getLogger(PDFMerger.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            for( PDDocument inputPDF : inputPDFs ){
+                try {
+                    pdfMerger.appendDocument( outputPDF, inputPDF );
+                    inputPDF.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(PDFMerger.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if( outputFilename != null ){
+                outputPDF.save( outputFilename );
+            }
+        }
+    }
+}
---
+++ src/main/java/edu/uab/cwingard/pdfeditor/PDFSplitter.java
@@ -0,0 +1,90 @@
+/*
+ * File: PDFSplitter.java
+ * Author: Blake Wingard, cwingard@uab.edu
+ * Assignment: group project - EE333 Spring 2020
+ * Vers: 1.0.0 03/29/2020 cbw - Initial coding
+ */
+package edu.uab.cwingard.pdfeditor;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.apache.pdfbox.multipdf.*;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+/**
+ * Edits PDF's
+ *
+ * @author Blake Wingard - cwingard@uab.edu
+ */
+public class PDFSplitter {
+
+    public static void main(String[] args) {
+        String filename;
+        String path;
+        int splitIndex;
+        File file;
+        ArrayList <String> filenames = new ArrayList<>();
+        PDDocument pdfDocument;
+        Splitter pdfSplitter;
+        PDFMergerUtility pdfMerger;
+        List< PDDocument> pdfDocuments;
+        int i = 0;
+        
+        if (args.length < 2 ) {
+            System.err.println("ERROR: Not enough arguments");
+        } else {
+            filename = args[ 0 ];
+            splitIndex = Integer.parseInt( args[ 1 ] );
+            if( splitIndex <= 0 ){
+                System.err.println( "ERROR: index must be non-negative." );
+                System.exit( -1 );
+            }
+            if( args.length >= 4 ){
+                filenames.add( args[ 2 ] );
+                filenames.add( args[ 3 ] );
+            } else {
+                filenames.add( "split1.pdf" );
+                filenames.add( "split2.pdf" );
+            }
+            try {
+                file = new File( filename );
+                path = file.getAbsolutePath().substring( 0, file.getAbsolutePath().lastIndexOf(File.separator));
+                pdfDocument = PDDocument.load(file);
+                pdfSplitter = new Splitter();
+                pdfMerger = new PDFMergerUtility();
+
+                pdfDocuments = pdfSplitter.split(pdfDocument);
+                for (int index = 1; index < splitIndex; ++index) {
+                    pdfMerger.appendDocument(pdfDocuments.get(0), pdfDocuments.get(1));
+                    pdfDocuments.get(1).close();
+                    pdfDocuments.remove(1);
+                }
+
+                while( 2 < pdfDocuments.size() ) {
+                    pdfMerger.appendDocument(pdfDocuments.get(1), pdfDocuments.get(2));
+                    pdfDocuments.get(2).close();
+                    pdfDocuments.remove(2);
+                }
+
+                for (PDDocument document : pdfDocuments) {
+                    System.out.println( path );
+                    document.save( path+ "/" + filenames.get( i ));
+                    document.close();
+                    ++i;
+                }
+
+            } catch (FileNotFoundException ex) {
+                System.err.println("ERROR: File not found.");
+            } catch (IOException ex) {
+                System.err.println("IOException");
+            }
+        }
+    }
+}
+
+
+
